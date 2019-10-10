acd161f00cf9e118c6581b3725926219c214be25
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-10-10 12:20:08 -0400

    fix: introducing env variable when available for Heroku deployment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import json2xls from "json2xls";
 import bodyParser from "body-parser";
 const app = express();
-
+let port = process.env.port || 3300;
 app.use(json2xls.middleware);
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 
@@ -10,4 +10,4 @@ app.post("/api/xlsx", (req, res) => {
   res.xls("hats.xlsx", req.body.hats);
 });
 
-app.listen(6000);
+app.listen(port);
