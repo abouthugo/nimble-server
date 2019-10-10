518522c43dc26a3698b9807bf10d4c9e2f9db3f0
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-10-10 12:52:26 -0400

    feat: cors protection and authorization

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import express from "express";
 import json2xls from "json2xls";
 import bodyParser from "body-parser";
-const app = express();
+import cors from "cors";
+
+const whitelist = JSON.parse(process.env.WHITELIST);
+const corsOpts = {
+  origin: (origin, cb) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      cb(null, true);
+    } else {
+      cb(new Error("Not allowed by CORS"));
+    }
+  }
+};
 const port = process.env.PORT || 3300;
+
+const app = express();
+app.use(cors(corsOpts));
 app.use(json2xls.middleware);
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 
